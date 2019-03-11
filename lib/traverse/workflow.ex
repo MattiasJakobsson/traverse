18305427b426069c78a8956a8ea3a373c13af9b0
Mattias Jakobsson <mattias@mattias-jakobsson.net>
2019-03-11 22:14:07 +0100

    Refactored steps

--- lib/traverse/workflow.ex
+++ lib/traverse/workflow.ex
@@ -1,55 +1,57 @@
 defmodule Traverse.Workflow do
   use GenServer
+  
+  defmodule Definition do
+    defstruct workflow_id: nil,
+              workflow_definition: %{},
+              current_state: %{}
+  end
 
   def start_workflow(definition, initial_state) do
-    workflow_id = UUID.uuid4()
+    workflow_definition = %Definition{workflow_id: UUID.uuid4(), workflow_definition: definition, current_state: initial_state}
 
-    {:ok, _} = GenServer.start_link(
+    GenServer.start_link(
       __MODULE__,
-      {definition, initial_state, workflow_id},
-      name: { :global, workflow_id }
+      workflow_definition,
+      name: { :global, workflow_definition.workflow_id }
     )
 
-    GenServer.cast({:global, workflow_id}, :start)
-
-    workflow_id
+    workflow_definition
   end
 
   def step_finished(workflow_id, step_definition, step_id, state, next_step) do
-    GenServer.cast({:global, workflow_id}, {:step_done, step_definition, step_id, state, next_step})
+    GenServer.cast({:global, workflow_id}, {:step_done, {step_definition, step_id, state, next_step}})
   end
 
-  def init(data) do
-    {:ok, data}
+  def init(definition) do
+    GenServer.cast(self(), :start)
+    
+    {:ok, definition}
   end
 
-  def handle_cast(:start, {definition, state, workflow_id}) do
-    execute_next_step(workflow_id, definition, state)
+  def handle_cast(:start, definition) do
+    Traverse.Steps.Step.start_step(definition.workflow_id, definition.step_definition, definition.current_state)
 
-    {:noreply, {definition, state, workflow_id}}
+    {:noreply, definition}
   end
 
-  def handle_cast({:step_done, step_definition, step_id, step_state, nil}, {definition, state, workflow_id}) do
+  def handle_cast({:step_done, {step_definition, step_id, step_state, nil}}, definition) do
     GenServer.stop({:global, step_id})
 
-    new_state = Map.put(state, step_definition.id, step_state)
+    new_definition = Map.put(definition, :current_state, Map.put(definition.current_state, String.to_atom(step_definition.id), step_state))
 
-    Traverse.Engine.workflow_finished(workflow_id, new_state)
+    Traverse.Engine.workflow_finished(new_definition.workflow_id, new_definition.current_state)
 
-    {:noreply, {definition, new_state, workflow_id}}
+    {:noreply, new_definition}
   end
 
-  def handle_cast({:step_done, step_definition, step_id, step_state, next_step}, {definition, state, workflow_id}) do
+  def handle_cast({:step_done, {step_definition, step_id, step_state, next_step}}, definition) do
     GenServer.stop({:global, step_id})
+    
+    new_definition = Map.put(definition, :current_state, Map.put(definition.current_state, String.to_atom(step_definition.id), step_state))
 
-    new_state = Map.put(state, step_definition.id, step_state)
+    Traverse.Steps.Step.start_step(definition.workflow_id, step_definition, new_definition.current_state)
 
-    execute_next_step(workflow_id, next_step, new_state)
-
-    {:noreply, {definition, new_state, workflow_id}}
-  end
-
-  def execute_next_step(workflow_id, step_definition, state) do
-    Traverse.Steps.Step.start_step(workflow_id, step_definition, state)
+    {:noreply, definition}
   end
 end
